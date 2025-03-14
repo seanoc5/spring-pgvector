e3aa3e8e4889672fcc29eeb7ae638e0f418f7654
Sean OC <seanoc5@gmail.com>
2025-03-14 11:44:56 -0400

    init-test-db.sql: add test approach to initializing pgvector SpringPgvectorApplicationTests: chatgpt suggestion for fixing tests

--- src/test/groovy/com/oconeco/spring_pgvector/SpringPgvectorApplicationTests.groovy
+++ src/test/groovy/com/oconeco/spring_pgvector/SpringPgvectorApplicationTests.groovy
@@ -2,12 +2,19 @@ package com.oconeco.spring_pgvector
 
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.beans.factory.annotation.Autowired
+import jakarta.persistence.EntityManager
 
 @SpringBootTest
 class SpringPgvectorApplicationTests {
 
+	@Autowired
+	private EntityManager entityManager
+
 	@Test
 	void contextLoads() {
+		// Basic test to ensure the application context loads successfully
+		assert entityManager != null
 	}
 
 }
