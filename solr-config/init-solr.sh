a74dfa5e72b361c34fda6943c2f7ac3bd21e35ae
Sean O'Connor <seanoc5@gmail.com>
2025-03-17 13:20:38 -0400

    adding config/setup for solr

---
+++ solr-config/init-solr.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+set -e
+
+echo "Solr initialization script started"
+
+# This script runs after Solr has created the collection using the precreate command
+# We can use it to perform any additional setup if needed
+
+echo "Solr initialization completed successfully!"
