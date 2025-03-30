a6285b56b2cc376a6f3967c8cf7c1e31ee0eeec3
Sean O'Connor <seanoc5@gmail.com>
2025-03-30 17:50:15 -0400

    Remove ./bin folder from repository

--- bin/main/test/RunCsvImporter.groovy
+++
@@ -1,8 +0,0 @@
-package test
-
-// This is a simple runner class to execute the CSV importer
-class RunCsvImporter {
-    static void main(String[] args) {
-        SamGovCsvImporter.main(args)
-    }
-}
